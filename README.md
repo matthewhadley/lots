d55715772a7f841e2a988ced5295e7b4856d6fcb
Matthew Hadley <matthew.hadley@gmail.com>
2012-02-26 01:20:14 -0800

    first commit

---
+++ README.md
@@ -0,0 +1,34 @@
+# Little Open Ticket System (LOTS)
+
+**LOTS** is a simple ticketing system for code inspired by [BANG](http://www.thecodebase.com/bang/).
+
+Using twitter style tagging, you can simply add a !tag and a ^priority to comments, and **LOTS** presents these for you in a purty interface.
+**LOTS** can also find any mention of the word _"TODO"_ in your code.
+
+### Installation
+
+    $ git clone git://github.com/diffsky/LOTS.git
+
+### Usage
+
+**LOTS** runs on [nodejs](http://nodejs.org/). In a directory you wish to review your code from:
+
+    $ node path/to/LOTS.js <port number> <find todos> <ndocco server>
+
+Options:
+
+   * `port number` - port for **LOTS** to run its server on
+   * `find todos` - by default, **LOTS** will search for "TODO" in your code, pass `false` to disable
+   * `ndocco server` - location of a running [ndocco](https://github.com/diffsky/ndocco) server (for example, `localhost:8888`)
+
+If a `ndocco server` location is passed then file links in the **LOTS** report will point to their ndocco output.
+
+Here is an example of a snippet that would be picked up by **LOTS**:
+
+    # This readme should be improved !documentation ^2
+
+### Hat Tip
+
+**LOTS** makes use of:
+
+   * [mustache](https://github.com/janl/mustache.js)
---
+++ lots.js
@@ -0,0 +1,216 @@
+/*
+# Little Open Ticket System (LOTS)
+
+**LOTS** is a simple ticketing system for code inspired by [BANG](http://www.thecodebase.com/bang/).
+
+**LOTS** greps the filesystem for twitter style tags and TODOs and displayes them in an HTML table.
+*/
+
+// ## **LOTS** setup
+
+/*
+Include the required node modules.
+
+* nodejs core modules
+* [mustache](https://github.com/janl/mustache.js)
+*/
+var path = require('path'),
+    fs = require('fs'),
+    http = require('http'),
+    url = require('url'),
+    util = require('util'),
+    exec = require('child_process').exec,
+    child,
+    Mustache = require('./mustache.js');
+
+// Object factory for ticket details
+function Ticket(id, file, line, comment, tag, priority) {
+    var ticket = {
+        'id' : id,
+        'file' : file,
+        'line' : line,
+        'comment' : comment,
+        'tag' : tag,
+        'priority' : priority,
+    };
+    return ticket;
+}
+
+/*
+## **LOTS** in action
+Behind the scenes, **LOTS** uses grep on the local filesystem to find strings that match:
+
+    `# These comments need updating !documentation ^3`
+
+    `# Replace mustache.js with more flexible Handlebars.js !templating ^1`
+
+And, optionally, any line that contains the word _"TODO"_
+
+The grep matches are then broken down into the actual comment, tags, priority, the file they
+belong to and the line they occur on. These are the Tickets. Tickets are then grouped by tags.
+*/
+
+// Handle the page request
+function onRequest(request, response) {
+    var ticketList = [],
+        tagGroup = {};
+    // Browser will also request a facivon. Don't do anything for for now.
+    var requestUrl = url.parse(request.url).pathname;
+    if (requestUrl === '/favicon.ico') {
+        return;
+    }
+    // ### Grep the file system for tags
+    // Grep results are in the format:
+    //
+    //    `[file]:[line number]:[grep match]`
+    //
+    // An empty trailing element is deleted
+    child = exec('grep -H -n -R -I \'#.*!.*^[0-9]\' *', function (err, stdout, stderr) {
+        matches = stdout.split("\n");
+        delete (matches[(matches.length - 1)]);
+
+        /*
+        Loop through all the matches and for each match take the first part as the file
+        name, the second part as the line number. Those entries are then deleted and the
+        remaining entries joined together to form the comment (this covers the case where
+        the comment might contain the grep deliminter).
+
+        The comment is then stripped of everything before the \# and the actual tag and
+        priority is extracted too.
+
+        These details are then used to create a new ticket object
+        */
+        var i;
+        for (i in matches) {
+            match = matches[i].split(":");
+            file = match[0];
+            line = match[1];
+            delete (match[0]);
+            delete (match[1]);
+            entry = match.join('');
+            bang = entry.match(/![a-zA-Z0-9]+ \^[0-9]+/, '');
+            tag = entry.match(/![a-zA-Z0-9]+/)[0].substring(1);
+            if (tagGroup[tag] === undefined) {
+                tagGroup[tag] = [];
+            }
+            tagGroup[tag].push(ticketList.length);
+            priority = entry.match(/\^[0-9]+/)[0].substring(1);
+            comment = entry.match(/#.*!/)[0];
+            comment = comment.substring(1, comment.length - 1);
+            ticketList[ticketList.length] = new Ticket(ticketList.length, file, line, comment, tag, priority);
+        }
+
+        // If we're not looking for _"TODO"_s then skip to building the LOTS object
+        if (findTodos === false) {
+            buildLOTS(response);
+        }
+
+        // ### Search the filesystem for _"TODO"_s
+        // Similar to the search for tags, a grep is performed, matches are found
+        // and then cleaned up.
+        //
+        // Tickets created here are assigned to the _TODO_ tag
+        child = exec('grep -H -n -R TODO *', function (err, stdout, stderr) {
+            matches = stdout.split("\n");
+            delete (matches[(matches.length - 1)]);
+
+            var i;
+            for (i in matches) {
+                match = matches[i].split(":");
+                file = match[0];
+                line = match[1];
+                delete (match[0]);
+                delete (match[1]);
+                if (tagGroup.TODO === undefined) {
+                    tagGroup.TODO = [];
+                }
+                tagGroup.TODO.push(ticketList.length);
+                comment = match.join('');
+                ticketList[ticketList.length] = new Ticket(ticketList.length, file, line, comment, 'TODO');
+            }
+            buildLOTS(response);
+        });
+    });
+
+    /*
+    ### Create the **LOTS** object
+    The found tag groups are looped through and each ticket in the group is added
+    to a tags array.
+    */
+    function buildLOTS(response) {
+        var LOTS = {};
+        LOTS.path = process.cwd();
+
+        if (ticketList.length > 0) {
+            var tags = [], tag;
+            var currentTicket;
+            var n = 1;
+            // Loop the tag groups.
+            for (tag in tagGroup) {
+                currentTag = tags.length;
+                tags[currentTag] = {};
+                tags[currentTag].name = tag;
+                tags[currentTag].tickets = [];
+                // loop the tickets in the tag group.
+                for (i = 0; i < tagGroup[tag].length; i++) {
+                    currentTicket = ticketList[tagGroup[tag][i]];
+                    currentTicket.number = i + 1;
+                    currentTicket.order = n++;
+                    // Needed due to logicless mustache templates :-/
+                    if (n % 2 === 1) {
+                        currentTicket.odd = 'odd';
+                    }
+                    tags[currentTag].tickets.push(currentTicket);
+                    console.log(ticketList[tagGroup[tag][i]]);
+                }
+                tags[currentTag].count = tags[currentTag].tickets.length;
+                // Needed due to logicless mustache templates :-/
+                tags[currentTag].tickets[tags[currentTag].tickets.length - 1].last = 1;
+            }
+            LOTS.tags = tags;
+            if (ndoccoServer !== undefined) {
+                LOTS.ndoccoServer = ndoccoServer;
+            }
+            console.log(LOTS);
+        } else {
+            LOTS.empty = 1;
+        }
+
+        // Render the **LOTS** object in the mustache template and send the response.
+        fs.readFile(lotsPath + 'lots.mustache', function (err, data) {
+            if (err) {
+                throw err;
+            }
+            var output = Mustache.render(data.toString(), LOTS);
+            response.writeHead(200, {"Content-Type": "text/html"});
+            response.write(output);
+            response.end();
+            return;
+        });
+    }
+}
+
+// ## **LOTS** server setup
+// Get the location that **LOTS** is running from so that it knows
+// where to include mustache template files.
+// process.argv[1] will contain the path to this file.
+var lotsPath = path.dirname(path.normalize(process.argv[1])) + '/';
+
+// Create the **LOTS** server.
+var port = 8888;
+if (process.argv[2] !== undefined) {
+    port = process.argv[2];
+}
+http.createServer(onRequest).listen(port);
+console.log('LOTS server started at http://localhost:' + port);
+
+// Determine if we are also looking for "TODO"s in the code.
+var findTodos = true;
+if (process.argv[3] !== undefined && process.argv[3] === 'false') {
+    findTodos = false;
+}
+// Capture the location of a ndocco server to use.
+if (process.argv[4] !== undefined) {
+    var ndoccoServer = process.argv[4];
+    console.log('Pairing with ndocco server at http://' + ndoccoServer);
+}
